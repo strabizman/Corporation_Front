5e46b7bb6ea1fea95d9608bab0624b46bfb68b63
strabizmanm <pierrepro.six@gmail.com>
2024-09-12 16:05:11 +0200

    v1 alpha

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
-
+import { HomeComponent } from './home/home.component';  // Composant Login
 import { RegisterComponent } from './register/register.component';  // Composant Register
-
+import { LoginComponent } from './login/login.component';  // Composant Login
+import { ShipsWikiComponent } from './ships-wiki/ships-wiki.component';  // Composant Login
 export const routes: Routes = [
   
   { path: 'register', component: RegisterComponent },  // Composant Register
+  { path: 'login', component: LoginComponent },  
+  { path: 'home', component: HomeComponent },
+  { path: 'wikiShips', component: ShipsWikiComponent },  
   
 ];
